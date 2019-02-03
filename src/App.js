03a124e8380ac83003ba3dbdb14ca66f3120b7f5
JayRodrig <mrjosemlrod@gmail.com>
2019-02-02 21:13:03 -0500

    added some files and layed out html and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TopRow from './components/toprow';
+import './app.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displayValue: '0',
+      previousValue: null,
+      operation: null,
+      waitingForNewValue: false
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+      <TopRow />
+      
+        <div className='container'>
+          <div className='row'>
+            <div className='col col-3 button'>AC</div>
+            <div className='col col-3 button'>%</div>
+            <div className='col col-3 button'>±</div>
+            <div className='col col-3 button orange'>÷</div>
+          </div>
+      
+          <div className='row'>
+            <div className='col col-3 button'>7</div>
+            <div className='col col-3 button'>8</div>
+            <div className='col col-3 button'>9</div>
+            <div className='col col-3 button orange'>x</div>
+          </div>
+      
+          <div className='row'>
+            <div className='col col-3 button'>4</div>
+            <div className='col col-3 button'>5</div>
+            <div className='col col-3 button'>6</div>
+            <div className='col col-3 button orange'>-</div>
+          </div>
+      
+          <div className='row'>
+            <div className='col col-3 button'>1</div>
+            <div className='col col-3 button'>2</div>
+            <div className='col col-3 button'>3</div>
+            <div className='col col-3 button orange'>+</div>
+          </div>
+      
+          <div className='row'>
+            <div className='col col-6 button'>0</div>
+            <div className='col col button'>.</div>
+            <div className='col col button orange'>=</div>
+          </div>
+        </div>
+      </>
     );
   }
 }
---
+++ src/components/toprow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TopRow = props => {
+    return (
+        <>
+            <div className='container'>
+                <div className='row'>
+                  <div className='col col-12 top-row'>0</div>
+                </div>
+            </div>
+        </>
+    );
+}   
+
+export default TopRow;
